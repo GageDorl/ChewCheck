670675b9cc8c927d3949e131702d1d84c5fd9953
Gage <gagedorlando@gmail.com>
2025-03-13 16:34:57 -0600

    got server api calls working

--- ChewCheck/src/script/stuff.js
+++ ChewCheck/src/script/stuff.js
@@ -1,7 +1,14 @@
-async function apiCall() {
-    const res = await fetch('http://localhost:5000/api/data');
-    const data = await res.json();
-    console.log(data.message);
+async function apiCall(keyword) {
+    try {
+        const res = await fetch(`http://localhost:5010/api/data?query=${keyword}`);
+        const data = await res.json();
+        console.log(data);
+    } catch(error) {
+        console.error("Error fetching food data:", error);
+    }
 }
 
-apiCall();
+document.querySelector('#searchButton').addEventListener('click', ()=>{
+    const keyword = document.querySelector('#searchInput').value;
+    apiCall(keyword);
+});
