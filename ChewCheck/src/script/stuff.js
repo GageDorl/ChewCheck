662f51c08e56def0339634bb4d18c344abcbde52
Sophie Archibald <arc21004@byui.edu>
2025-03-13 10:14:42 -0600

    Updated port number

--- ChewCheck/src/script/stuff.js
+++ ChewCheck/src/script/stuff.js
@@ -1,5 +1,5 @@
 async function apiCall() {
-    const res = await fetch('http://localhost:5000/api/data');
+    const res = await fetch('http://localhost:5010/api/data');
     const data = await res.json();
     console.log(data.message);
 }
--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.get('/api/data', async (req, res) => {
     res.json({ message: 'Hello from the server!' });
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(5010, () => {
+    console.log('Server is running on port 5010');
 });
