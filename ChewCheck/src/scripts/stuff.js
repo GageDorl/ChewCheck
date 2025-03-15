d0979e598778bdb5d5312525bf11a6ef3b9faca1
Gage <gagedorlando@gmail.com>
2025-03-15 14:54:52 -0600

    got a domain

--- ChewCheck/src/scripts/stuff.js
+++ ChewCheck/src/scripts/stuff.js
@@ -1,6 +1,6 @@
 async function apiCall(keyword) {
     try {
-        const res = await fetch(`https://69.62.65.213:9000/api/data?query=${keyword}`);
+        const res = await fetch(`https://chewcheck.tech:9000/api/data?query=${keyword}`);
         const data = await res.json();
         console.log(data);
     } catch(error) {
